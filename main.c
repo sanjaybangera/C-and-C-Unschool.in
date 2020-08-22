56b2cb605d2d59f85b41b26b9df6332843ffe098
sanjaybangera <69789543+sanjaybangera@users.noreply.github.com>
2020-08-22 19:48:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void  main()
+{
+int sum,n1,n2,n3;
+printf("enter 3 number");
+    scanf("%d%d%d",&n1,&n2,&n3);
+    sum=n1+n2+n3;
+    printf("sum of %d+%d+%d is %d",n1,n2,n3,sum);
+}
